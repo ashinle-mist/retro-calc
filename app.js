538114dbc39f5c950f097fc8f5051f86873f9294
Ashinle Mist <raiyanap19@gmail.com>
2025-07-04 14:30:05 +1000

    Implement switch case handler and test addition and clear functions

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const calculator = document.getElementById('calculator')
+let currentInput = '';
 
 calculator.addEventListener('click', (e) => {
     const numpad = e.target;
 
-    if (!numpad.matches('numpad')) return;
+    if (!numpad.matches('.numpad')) return;
 
     const action = numpad.dataset.action;
     const value = numpad.dataset.value;
 
-    handleButtonClick(action, value);
+    // updateDisplay(value)
+    handleButtonClick(action, value)
 })
 
-function handleButtonClick(action, value) {
-
+function updateDisplay(value) {
+    const display = document.getElementById('display')
+    display.textContent = value || '0';
 }
+
+function handleButtonClick(action, value) {
+    switch (action) {
+        case 'number':
+            addNumber(value);
+            break;
+        case 'clear':
+            clear();
+            break;
+    }
+}
+
+function addNumber(num) {
+    currentInput += num;
+    updateDisplay(currentInput);
+}
+
+function clear() {
+    currentInput = '';
+    updateDisplay(0);
+}
+
